5f049ba79bb9cc2b72037c9d43c3a99fbf6b54fd
Darinochka <darisha6602@gmail.com>
2021-06-04 01:01:51 +0300

    table

--- main.py
+++ main.py
@@ -11,8 +11,14 @@ import tkinter.ttk as ttk
 
 #     def __init__(self, df):
         
+class table(ttk.Frame):
 
-class Table(ttk.Frame):
+    def __init__(self, parent, df):
+        super().__init__(parent)
+        self.df = df
+
+        self.grid(column=0, row=0)
+class table_manage(ttk.Frame):
  
     def __init__(self, parent, df):
         super().__init__(parent)
@@ -20,40 +26,61 @@ class Table(ttk.Frame):
 
         self.grid(column=0, row=0)
 
-        frame = ttk.Frame(self)
+        hight = df.shape[0]
+        weight = df.shape[1]
+
+        widgets_entry = np.empty(shape=(hight, weight), dtype="O")
+        values_df = np.empty(shape=(hight, weight), dtype="O")
+        rows_numbers = np.empty(shape=hight, dtype="O")
+       
+        #frame with table
+        frame = ttk.Frame(self, relief="ridge")
         frame.grid(column=0, row=0, sticky="nsew")
 
-        my_canvas = tk.Canvas(frame)
-        my_canvas.pack(side=tk.LEFT, fill=tk.BOTH)
+        #creation canvas for scrollbar
+        canvas = tk.Canvas(frame)
+        scrollbar_x = ttk.Scrollbar(frame, orient=tk.HORIZONTAL, command=canvas.xview)
+        scrollbar_y = ttk.Scrollbar(frame, orient=tk.VERTICAL, command=canvas.yview)
+        sizegrip = ttk.Sizegrip(frame)
 
-        my_scrollbar = ttk.Scrollbar(frame, orient=tk.VERTICAL, command=my_canvas.yview)
-        my_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        scrollable_frame = ttk.Frame(canvas)
+        scrollable_frame.bind('<Configure>', lambda e: canvas.configure(scrollregion = canvas.bbox("all")))
 
-        my_canvas.configure(yscrollcommand=my_scrollbar.set)
-        my_canvas.bind('<Configure>', lambda e: my_canvas.configure(scrollregion = my_canvas.bbox("all")))
+        canvas.create_window((0,0), window=scrollable_frame, anchor="nw")
+        canvas.configure(xscrollcommand=scrollbar_x.set, yscrollcommand=scrollbar_y.set)
         
-        second_frame = ttk.Frame(my_canvas)
-        my_canvas.create_window((0,0), window=second_frame, anchor="nw")
+        scrollbar_x.pack(side="bottom", fill="x")
+        scrollbar_y.pack(side="right", fill="y")
+        sizegrip.pack(in_ = scrollbar_x, side="bottom", anchor="se")
+        canvas.pack(side="left", fill="both", expand=True)
+        
+        for i in range(hight): 
+            for j, column in enumerate(df.columns):
 
+                columns_label = ttk.Label(scrollable_frame, text=column)
+                columns_label.grid(row=0, column=j+1)
+
+                values_df[i, j] = tk.StringVar()
+
+                if j == 0:
+                    rows_numbers[i] = ttk.Label(scrollable_frame, text=i)
+                    rows_numbers[i].grid(row=i+1, column=j)
+                
+                widgets_entry[i, j] = tk.Entry(scrollable_frame, textvariable = values_df[i, j])
+                widgets_entry[i, j].grid(row=i+1, column=j+1)
+
+                cnt = df.iloc[i, j]
+                values_df[i, j].set(str(cnt))
+                # cell = ttk.Entry(scrollable_frame)
+                # cell.insert(0,self.df[column][j])
+                # cell.grid(row=j+1, column=i+1)
+                
+        #editor table
         menu = ttk.Frame(self, relief="ridge")
         menu.grid(column=1, row=0, sticky="nsew")
+        
 
-        for i, column in enumerate(self.df.columns):
-            second_frame.grid_columnconfigure(i, weight=1)
-            second_frame.grid_rowconfigure(i, weight=1)
-
-            column_label = ttk.Label(second_frame, text=column)
-            column_label.grid(row=0, column=i+1)
-            for j in range(len(df.index)):
-                
-                if i == 0:
-                    num = ttk.Label(second_frame, text=j)
-                    num.grid(row=j+1, column=i)
-
-                cell = ttk.Entry(second_frame)
-                cell.insert(0,self.df[column][j])
-                cell.grid(row=j+1, column=i+1)
-
+        #config grod of the main frame
         self.grid_columnconfigure(0, weight=1, uniform="group1")
         self.rowconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1, uniform="group1")
@@ -70,10 +97,10 @@ def main():
     root.geometry('1000x600+200+150')
     n = ttk.Notebook(root, width=root.winfo_screenwidth(), height=root.winfo_screenheight())
 
-    tracks = Table(root, df_tracks)
-    albums = Table(root, df_albums)
-    artists = Table(root, df_artists)
-    genres = Table(root, df_genres)
+    tracks = table_manage(root, df_tracks)
+    albums = table_manage(root, df_albums)
+    artists = table_manage(root, df_artists)
+    genres = table_manage(root, df_genres)
 
 
     n.add(tracks, text='Треки')
